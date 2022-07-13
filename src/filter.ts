e2a9a6dad8c2bb09c554875371fe676f97bb3908
Wilrosmi <will.r.smith31@gmail.com>
2022-07-13 11:28:30 +0100

    Formatting for lint and prettier

--- src/filter.ts
+++ src/filter.ts
@@ -1,14 +1,12 @@
 /**
  * Filters each element of an array out if a given function returns false for that element
- * 
  * @param arr - the array to filter using func
  * @param func - the function to decide which array elements to filter
- * 
  * @returns the filtered version of arr, using func
  */
 function filter<T>(arr: T[], func: (arg: T) => boolean): T[] {
   const outArr = [];
-  for (let element of arr) {
+  for (const element of arr) {
     if (func(element)) {
       outArr.push(element);
     }
